d5bd44c98750218345b2f2587214bc7aad70f2c7
Seu nome <rodrigoarcanjo690@gmail.com>
2024-01-30 11:54:47 -0300

    loja virtual

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Rodape = () => {
+  return (
+    <footer className="text-center bg-black text-bg-dark">
+      <p className="card-text py-3">
+        2023 <i className="bi bi-c-circle"></i> Desenvolvido por Rodrigo Arcanjo.
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
